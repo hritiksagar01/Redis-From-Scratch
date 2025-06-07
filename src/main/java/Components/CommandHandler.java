f5fbe56190136d5b76896841b05969196937cc71
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-06-07 21:18:51 +0530

    #la7

--- src/main/java/Components/CommandHandler.java
+++ src/main/java/Components/CommandHandler.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-@Component
+
 public class CommandHandler {
     @Autowired
     public Components.Service.RespSerializer respSerializer;
