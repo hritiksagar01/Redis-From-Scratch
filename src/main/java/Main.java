eff2b07b2ca5fdbca8e18b0a3f714b13077e9101
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-06-07 16:29:47 +0530

    wy1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,42 +5,60 @@ import Config.AppConfig;
 import Components.Server.MasterTcpServer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Logs from your program will appear here!");
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        MasterTcpServer master = context.getBean(MasterTcpServer.class);
-        SlaveTcpServer slave = context.getBean(SlaveTcpServer.class);
-
-        RedisConfig redisConfig = context.getBean(RedisConfig.class);
-        int port= 6379;
-        redisConfig.setPort(port);;
-
-        redisConfig.setRole("master");
-        for(int i =0 ; i< args.length; i++){
-           switch (args[i]){
-               case "--port":
-                   port = Integer.parseInt(args[i+1]);
-                   redisConfig.setPort(port);
-                   break;
-                   case "--replicaof":
-                       redisConfig.setRole("slave");
-                       String masterHost = args[i+1].split(" ")[0];
-                          int masterPort = Integer.parseInt(args[i+1].split(" ")[1]);
-                          redisConfig.setMasterHost(masterHost);
-                            redisConfig.setMasterPort(masterPort);
-                          break;
-           }
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try{
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
+            OutputStream outputStream = clientSocket.getOutputStream();
+             outputStream.write("+PONG\r\n".getBytes());
+        } catch (Exception e) {
+            System.out.println("Error creating server socket: " + e.getMessage());
+            return;
         }
-        redisConfig.setPort(port);
-        redisConfig.setRole("master");
-        if(redisConfig.getRole().equals("slave")) {
-            slave.startServer();
-        } else {
-            master.startServer(6379);
-        }
-        master.startServer(6379);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        MasterTcpServer master = context.getBean(MasterTcpServer.class);
+//        SlaveTcpServer slave = context.getBean(SlaveTcpServer.class);
+//
+//        RedisConfig redisConfig = context.getBean(RedisConfig.class);
+//        int port= 6379;
+//        redisConfig.setPort(port);;
+//
+//        redisConfig.setRole("master");
+//        for(int i =0 ; i< args.length; i++){
+//           switch (args[i]){
+//               case "--port":
+//                   port = Integer.parseInt(args[i+1]);
+//                   redisConfig.setPort(port);
+//                   break;
+//                   case "--replicaof":
+//                       redisConfig.setRole("slave");
+//                       String masterHost = args[i+1].split(" ")[0];
+//                          int masterPort = Integer.parseInt(args[i+1].split(" ")[1]);
+//                          redisConfig.setMasterHost(masterHost);
+//                            redisConfig.setMasterPort(masterPort);
+//                          break;
+//           }
+//        }
+//        redisConfig.setPort(port);
+//        redisConfig.setRole("master");
+//        if(redisConfig.getRole().equals("slave")) {
+//            slave.startServer();
+//        } else {
+//            master.startServer(6379);
+//        }
+//        master.startServer(6379);
     }
 }
 
